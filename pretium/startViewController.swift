3667463d42853cd981983fbdd74e3f378906e3e8
Daniel Kerrigan <dkerriga@nd.edu>
2016-02-20 12:55:59 -0800

    Added icon. Fixed transition from start button to camera.

---
+++ pretium/startViewController.swift
@@ -0,0 +1,17 @@
+//
+//  startViewController.swift
+//  pretium
+//
+//  Created by Dan Kerrigan on 2/20/16.
+//  Copyright © 2016 ndSophware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class startViewController: UIViewController {
+    
+   
+    
+    
+}
