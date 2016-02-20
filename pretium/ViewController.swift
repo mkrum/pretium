f1ad67076a5c160ba89767b3aa8fefe59158ac95
mkrum <mrkrumdick@gmail.com>
2016-02-20 12:28:48 -0600

    scaling image

--- pretium/ViewController.swift
+++ pretium/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         var newImage: UIImage
         if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             newImage = possibleImage
-            readText(newImage)
+            readText(scaleImage(newImage, maxDimension: 640))
         } else {
             print("Error")
             return
@@ -75,6 +75,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         tesseract.image = image.g8_blackAndWhite()
         tesseract.recognize()
         print(tesseract.recognizedText)
+    }
+    
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
     }
     
     func cleanString(mess: String)-> [String] {
