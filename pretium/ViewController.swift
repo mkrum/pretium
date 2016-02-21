f7731cb59e9cd3eb3e2410452161b3cd716904a1
Luke Duane <lukeduane@wirelessprvnat-172-17-87-45.near.illinois.edu>
2016-02-21 00:11:44 -0600

    Dope Dollar Animation

--- pretium/ViewController.swift
+++ pretium/ViewController.swift
@@ -125,6 +125,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
--- pretium/startViewController.swift
+++ pretium/startViewController.swift
@@ -11,10 +11,22 @@ import UIKit
 
 class startViewController: UIViewController {
     
+    @IBOutlet weak var cashImage2: UIImageView!
     var prices:[String] = []
     
+
+    @IBOutlet weak var cashImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        var imageArray = [UIImage]()
+        for var x=1; x<=2; x++ {
+            let img = UIImage(named: "cash\(x).png")
+            imageArray.append(img!)
+        }
+        cashImage2.animationImages=imageArray
+        cashImage2.animationDuration=0.8
+        cashImage2.animationRepeatCount=0
+        cashImage2.startAnimating()
     }
     
 }
